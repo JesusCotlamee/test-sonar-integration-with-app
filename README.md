ec36d35bba2e47bc62cef0acbaa558122ea48ccb
Jesus Cotlame <jcotlame@iwa.com.mx>
2024-03-15 18:27:22 -0600

    feat(action): update logs
    Author: JesusCotlame

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test-sonar-integration-with-app
+Project to use the github app of sonar
---
+++ src/main/java/pixee/Main.java
@@ -0,0 +1,31 @@
+package pixee;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static int myIntegerValue;
+
+    public static void main(String[] args) {
+        myIntegerValue = Integer.parseInt("3");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+
+    public void log(){
+        logger.info(String.format("Processing annotations %d", myIntegerValue));
+    }
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+}
