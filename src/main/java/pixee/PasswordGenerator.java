ffba6e0eadd47fbbe532aef3ec3c64b6ca151c22
Jesus Cotlame <jcotlame@iwa.com.mx>
2024-03-15 18:39:04 -0600

    feat(action): add java class
    Author: JesusCotlame

---
+++ src/main/java/pixee/PasswordGenerator.java
@@ -0,0 +1,17 @@
+package pixee;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class PasswordGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+
+    public static String generatePassword(int length) {
+        StringBuilder password = new StringBuilder();
+        Random random = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            password.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return password.toString();
+    }
+}
